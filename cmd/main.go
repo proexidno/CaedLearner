4a5cc8684e6d6c361a31b4ec3b9c993972dfa454
proexidno <fierydragoon6@gmail.com>
2024-10-19 14:55:37 +0700

    import token

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/mymmrac/telego"
+)
+
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
+	}
+
+	botToken := os.Getenv("TELEGRAM_API_TOKEN")
+
+	println(botToken)
+
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+
+	if err != nil {
+		fmt.Println("No telegram token")
+		os.Exit(1)
+	}
+
+	println(bot)
 	return
 }
